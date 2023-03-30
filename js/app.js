e83723e043aa6c5a05d309c900e4fdfc50a58da4
lipengyu <lipengyu2006@gmail.com>
2023-03-30 16:12:12 +0800

    adjust to small screen

--- js/app.js
+++ js/app.js
@@ -5,11 +5,7 @@
     await Player5({
       playerWrapperElm: player,
       contentElm: content,
-      playListURL: './data/chapter3.json'
-      //playListURL: './data/chapter4.json'
-      //playListURL: './data/chapter5.json'
-      //playListURL: './data/chapter6.json'
-      //playListURL: './data/ted.json'
+      sortListURL: './data/app.json'
     });
   });
 })();
